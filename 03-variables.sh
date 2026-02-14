c5c238fdc0af2c57ab0893c3e497baeeb51eb099
bhargav-founder <bhargavchitra05.com>
2026-02-14 08:11:33 +0530

    actions

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,10 +1,23 @@
+
 #!/bin/bash
 
 
-PERSON1=putin
-PERSON2=trump
+PERSON1=$1
+PERSON2=$2
 
 echo "$PERSON1:: Hello Ramesh, How are you?"
 echo "$PERSON2:: Hi Suresh, I am fine thanks for asking. How are you doing?"
 echo "$PERSON1:: doing fine. What are you learning $PERSON2?"
 echo "$PERSON2:: I am learning Shell. What about you?"
+
+
+
+
+
+
+
+
+
+
+
+
