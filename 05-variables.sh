3ee19986df8f46a55c452f3eaf812c21bacf558b
bhargav-founder <bhargavchitra05.com>
2026-02-14 10:58:22 +0530

    actions

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-TIMESTAMP=$(date)
+START_TIME=$(date +%s)
 
 echo "Script executed at: $TIMESTAMP"
+
+sleep 10
+
+END_TIME=$(datee date +%s)
+TOTAL_TIME=$(($END_TIME-$START_TIME))
+
+echo "script executed in: $TOTAL_TIME"
 
 
 
