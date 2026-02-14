b2c6e6d7f73b1d84452211ed1a8387560f0c14b9
bhargav-founder <bhargavchitra05.com>
2026-02-14 16:42:35 +0530

    actions

--- 08-conditionals.sh
+++ 08-conditionals.sh
@@ -6,7 +6,7 @@ NUMBER=25
 #-lt -> less than
 #-eq -> equal 
 #-ne -> not equal
-if [$NUMBER -gt 20]; then
+if [ $NUMBER -gt 20 ]; then
      echo "Given number: $NUMBER is greater than 25"
      fi
 
