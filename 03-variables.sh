581f35fa2bd157358955ebbb36c3f4ee0f6e4cca
bhargav-founder <bhargavchitra05.com>
2026-02-15 12:00:41 +0530

    action

--- 03-variables.sh
+++ 03-variables.sh
@@ -5,8 +5,8 @@
 PERSON1=$1
 PERSON2=$2
 
-echo "$PERSON1:: Hello Ramesh, How are you?"
-echo "$PERSON2:: Hi Suresh, I am fine thanks for asking. How are you doing?"
+echo "$PERSON1:: Hello $PERSON2, How are you?"
+echo "$PERSON2:: Hi $PERSON1, I am fine thanks for asking. How are you doing?"
 echo "$PERSON1:: doing fine. What are you learning $PERSON2?"
 echo "$PERSON2:: I am learning Shell. What about you?"
 
