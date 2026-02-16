8943250549431cdf9587843bf6828636a724c179
bhargav-founder <bhargavchitra05.com>
2026-02-16 11:19:01 +0530

    action

--- 11-logs.sh
+++ 11-logs.sh
@@ -6,20 +6,21 @@ LOGS_FILE="/var/log/shell-script/$0.log"
 
 
 if  [ $USERID -ne 0 ]; then
-    echo "please run this script with root user access"
+    echo "please run this script with root user access" | tee -a $LOGS_FILE
     exit 1
 fi
 
 VALIDATE(){
     if [ $1 -ne 0 ]; then
-        echo  "$2 .... FAILURE"
+        echo  "$2 .... FAILURE" | tee -a $LOGS_FILE
         exit 1
     else
-        echo  "$2 .... SUCCESS"
+        echo  "$2 .... SUCCESS" | | tee -a $LOGS_FILE
     fi 
 
 
 }
+
 dnf install nginx -y &>> $LOGS_FILE
 VALIDATE $? "installing nginx"
 
