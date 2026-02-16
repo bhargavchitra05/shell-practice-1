0d37c3e552fb6c432413e4bf014c9ecc53bcd409
bhargav-founder <bhargavchitra05.com>
2026-02-16 21:25:15 +0530

    action

--- 17-set.sh
+++ 17-set.sh
@@ -36,7 +36,7 @@ do
     if [ $? -ne 0 ]; then
     echo "$package not installed, installing now"
      dnf install $package -y &>>$LOGS_FILE
-    VALIDATE $? "$package installation"
+    #VALIDATE $? "$package installation"
     else
         echo "$package already installed ......$Y skipping $N"
     fi
