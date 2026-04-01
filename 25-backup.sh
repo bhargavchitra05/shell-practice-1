fc39707ade629774551f30c9cf10dfe832bf8294
bhargav-founder <bhargavchitra05.com>
2026-04-01 20:11:12 +0530

    actions

--- 25-backup.sh
+++ 25-backup.sh
@@ -2,15 +2,22 @@
 
 USERID=$(id -u)
 LOGS_FOLDER="/var/log/shell-script"
-LOGS_FILE="/var/log/shell-script/$0.log"
+LOGS_FILE="/var/log/shell-script/backup.log"
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
+SOURCE_DIR=$1
+DEST_DIR=$2
+DAYS=${3:-14} # 14 days is the default value, if the user not supplied
 
+log(){
+    echo -e "$(date "+%Y-%m-%d %H:%M:%S") | $1" | tee -a $LOGS_FILE
+}
 
 if [ $USERID -ne 0 ]; then
     echo -e "$R Please run this script with root user access $N"
+    exit 1
 fi
 
 mkdir -p $LOGS_FOLDER
@@ -20,6 +27,12 @@ USAGE(){
     exit 1
 }
 
+
 if [ $# -lt 2 ]; then
     USAGE
+fi
+
+if [ ! -d "$SOURCE_DIR" ]; then
+    log "$R Source Directory: $SOURCE_DIR does not exist $N"
+    exit 1
 fi
