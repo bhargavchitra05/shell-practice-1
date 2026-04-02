f94e7f667b9ff6cc46f38c8744c0881726e6173f
bhargav-founder <bhargavchitra05.com>
2026-04-02 23:44:51 +0530

    actions

---
+++ 26-disk-usage.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+MESSAGE=""
+
+log(){
+    echo -e "$(date "+%Y-%m-%d %H:%M:%S") | $1" | tee -a $LOGS_FILE
+}
+
+DISK_USAGE=$(df -hT | grep -v Filesystem)
+USAGE_THRESHOLD=3
+
+while IFS= read -r line
+do
+    USAGE=$(echo $line | awk '{print $6}' | cut -d "%" -f1)
+    PARTITION=$(echo $line | awk '{print $7}')
+
+    if [ "$USAGE" -ge "$USAGE_THRESHOLD" ]; then
+        MESSAGE+="High Disk usage on $PARTITION: $USAGE% <br>"
+    fi
+done <<< $DISK_USAGE
+
+echo "$MESSAGE"
+
